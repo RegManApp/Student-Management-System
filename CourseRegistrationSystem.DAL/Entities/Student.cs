ad5899e61e60f9a82e63b097486c78eab4800285
Mosaab Gamal <hello@mosaab.is-a.dev>
2025-11-28 01:30:29 +0200

    edit Student class

--- CourseRegistrationSystem.DAL/Entities/Student.cs
+++ CourseRegistrationSystem.DAL/Entities/Student.cs
@@ -1,36 +1,34 @@
 using System.ComponentModel.DataAnnotations;
-using CourseRegistrationSystem.DAL.Entities;
 
-namespace CourseRegistrationSystem.DAL.Entities
+namespace CourseRegistrationSystem.DAL.Entities;
+
+public class Student : User
 {
-    public class Student : User
+    private string Address { get; set; } = string.Empty;
+    private string PhoneNumber { get; set; } = string.Empty;
+    [Required] private AcademicPlan AcademicPlan { get; set; } = new AcademicPlan();
+    [Required] private string FamilyContact { get; set; } = string.Empty;
+    private double GPA { get; set; } = 0.0;
+    private int CompletedCredits { get; set; } = 0;
+    private int RegisteredCredits { get; set; } = 0;
+
+    public bool RegisterSection(Section section)
     {
-        private string Address { get; set; } = string.Empty;
-        private string PhoneNumber { get; set; } = string.Empty;
-        [Required] private AcademicPlan AcademicPlan { get; set; } = new AcademicPlan();
-        [Required] private string FamilyContact { get; set; } = string.Empty;
-        private double GPA { get; set; } = 0.0;
-        private int CompletedCredits { get; set; } = 0;
-        private int RegisteredCredits { get; set; } = 0;
-        
-        public Boolean RegisterSection(Section section)
-        {
-            return true;
-        }
-        
-        public Boolean DropSection(Section section)
-        {
-            return true;
-        }
-        
-        public Transcript ViewTranscript()
-        {
-            return new Transcript();
-        }
-        
-        public double CalculateGPA()
-        {
-            return 0.0;
-        }
+        return true;
+    }
+
+    public bool DropSection(Section section)
+    {
+        return true;
+    }
+
+    public Transcript ViewTranscript()
+    {
+        return new Transcript();
+    }
+
+    public double CalculateGPA()
+    {
+        return 0.0;
     }
 }
