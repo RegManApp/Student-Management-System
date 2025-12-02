a2760b65ed1db023592af98f78ae3de3f299defa
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2025-12-02 14:55:02 +0200

    Added basic CRUD methods to ICourseService. Set return types for creation & updating to VIewCourseDetailsDTO to review changes that user has made. Deletion returns a string message for now.

---
+++ CourseRegistrationSystem.BusinessLayer/Contracts/ICourseService.cs
@@ -0,0 +1,22 @@
+﻿using StudentManagementSystem.BusinessLayer.DTOs.CourseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.BusinessLayer.Contracts
+{
+    public interface ICourseService
+    {
+        //Read
+        Task<IEnumerable<ViewCourseSummaryDTO>> GetAllCoursesAsync();
+        Task<ViewCourseDetailsDTO> GetCourseById(int id);
+        //Create
+        Task<ViewCourseDetailsDTO> CreateCourse(CreateCourseDTO courseDTO);
+        //Update
+        Task<ViewCourseDetailsDTO> UpdateCourse(UpdateCourseDTO courseDTO);
+        //Delete
+        Task<string> DeleteCourse(int id);
+    }
+}
