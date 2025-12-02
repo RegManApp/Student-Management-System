d658ab368eaba05c49398e895b3c8ee699781627
AhmedAmgad1 <meuser725@gmail.com>
2025-12-02 20:01:44 +0200

    Implement Enrollment class with status management
    Added properties and methods for enrollment management.

--- CourseRegistrationSystem.DAL/Entities/Enrollment.cs
+++ CourseRegistrationSystem.DAL/Entities/Enrollment.cs
@@ -5,8 +5,54 @@ namespace StudentManagementSystem.Entities
 {
     public class Enrollment
     {
-        //Implement class
+        
+        public Status Status {get;set;}
+        
+        [ForeignKey(nameof(StudentId))]
+        public Student StudentId;
 
+        [ForeignKey(nameof(SectionId))]
+        public Section SectionId;
+
+        // Operations
+        
+        public void ChangeStatus(Status newStatus)
+        {
+            Status = newStatus;
+        }
+
+
+        public bool IsActive()
+        {
+            return Status == Status.Enrolled;
+        }
+
+
+    public void Drop()
+    {
+        Status = Status.Dropped;
     }
 
+    }
 }
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    modelBuilder.Entity<Enrollment>()
+        .HasKey(e => new { e.StudentId, e.SectionId });
+
+    modelBuilder.Entity<Enrollment>()
+        .HasOne(e => e.Student);
+        .WithMany(s => s.Enrollments);
+        .HasForeignKey(e => e.StudentId);
+
+    modelBuilder.Entity<Enrollment>()
+        .HasOne(e => e.Section)
+        .WithMany(sec => sec.Enrollments);
+        .HasForeignKey(e => e.SectionId);
+
+    base.OnModelCreating(modelBuilder);
+}
+public enum Status{Enrolled, Dropped, Completed};
+
+public ICollection<Enrollment> Enrollments {get;set;}
+
