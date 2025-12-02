adbb3cccc594bc3b902fa0e69f484af6dca7b589
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2025-12-02 15:01:50 +0200

    Created CourseService in "Services" folder, and implemented ICourseService.

---
+++ CourseRegistrationSystem.BusinessLayer/Services/CourseService.cs
@@ -0,0 +1,38 @@
+﻿using StudentManagementSystem.BusinessLayer.Contracts;
+using StudentManagementSystem.BusinessLayer.DTOs.CourseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.BusinessLayer.Services
+{
+    internal class CourseService : ICourseService
+    {
+        public Task<ViewCourseDetailsDTO> CreateCourse(CreateCourseDTO courseDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteCourse(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ViewCourseSummaryDTO>> GetAllCoursesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ViewCourseDetailsDTO> GetCourseById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ViewCourseDetailsDTO> UpdateCourse(UpdateCourseDTO courseDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
