e014c12fee624aa6f99bd74e349dd12ccb3f7be8
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2025-12-03 15:36:49 +0200

    Feature: Created Course controller and Injected dependency for course service.

---
+++ StudentManagementSystem.API/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.BusinessLayer.Contracts;
+
+namespace StudentManagementSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService courseService;
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+    }
+}
