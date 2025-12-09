5995876f22bde21bbd7daa7950d430a3fc246690
A7MED970 <A.Shaban2388@nu.edu.eg>
2025-12-09 02:35:07 +0200

    fix API project NuGet packages and add JwtBearer reference

--- StudentManagementSystem.API/Program.cs
+++ StudentManagementSystem.API/Program.cs
@@ -1,6 +1,12 @@
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using StudentManagementSystem.BusinessLayer;
+using StudentManagementSystem.BusinessLayer.Services;
 using StudentManagementSystem.DAL;
+using StudentManagementSystem.DAL.DataContext;
+using StudentManagementSystem.Entities;
+using System.Text;
 
 namespace StudentManagementSystem.API
 {
@@ -10,17 +16,60 @@ namespace StudentManagementSystem.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddDataBaseLayer(builder.Configuration);  
+
+            // Add Database Layer + Business Services
+            builder.Services.AddDataBaseLayer(builder.Configuration);
             builder.Services.AddBusinessServices();
+
+
+            // Add Identity (Strong Password Rules)
+            builder.Services.AddIdentity<BaseUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 8;
+
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+            // Add JWT Authentication
+            var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+            // Register TokenService
+            builder.Services.AddScoped<TokenService>();
+
+            // Controllers + Swagger
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Swagger only in Development
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -29,8 +78,9 @@ namespace StudentManagementSystem.API
 
             app.UseHttpsRedirection();
 
+            // Order is IMPORTANT
+            app.UseAuthentication();  // MUST COME BEFORE Authorization
             app.UseAuthorization();
-
 
             app.MapControllers();
 
