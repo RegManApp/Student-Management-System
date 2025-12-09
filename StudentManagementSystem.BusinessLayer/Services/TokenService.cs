848c1c58e6060eb547a5f384c4ee543c64d360dc
A7MED970 <A.Shaban2388@nu.edu.eg>
2025-12-09 02:23:15 +0200

    implement JWT token service

---
+++ StudentManagementSystem.BusinessLayer/Services/TokenService.cs
@@ -0,0 +1,47 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using StudentManagementSystem.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StudentManagementSystem.BusinessLayer.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration config;
+
+        public TokenService(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public string GenerateToken(BaseUser user, IList<string> roles)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email ?? ""),
+                new Claim("fullname", user.FullName),
+                new Claim("role", roles.FirstOrDefault() ?? "")
+            };
+
+            // Add all roles (if multiple)
+            foreach (var role in roles)
+                claims.Add(new Claim(ClaimTypes.Role, role));
+
+            var token = new JwtSecurityToken(
+                issuer: config["Jwt:Issuer"],
+                audience: config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(config["Jwt:DurationInMinutes"]!)),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
