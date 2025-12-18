0154fed49ff5a20591709ed21e3874853bbe6b50
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2025-12-19 00:05:02 +0200

    feat: Admin can now create new student accounts. Authorized users can find other student accounts by ID

---
+++ StudentManagementSystem.API/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.API.Common;
+using StudentManagementSystem.BusinessLayer.Contracts;
+using StudentManagementSystem.BusinessLayer.DTOs.StudentDTOs;
+
+namespace StudentManagementSystem.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentProfileService studentProfileService;
+        public StudentController(IStudentProfileService studentProfileService) 
+        {
+            this.studentProfileService = studentProfileService;
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> CreateStudentAsync(CreateStudentDTO studentDTO) 
+        {
+            var result = await studentProfileService.CreateProfileAsync(studentDTO);
+            return Ok(ApiResponse<ViewStudentProfileDTO>.SuccessResponse(result));
+        }  
+        [HttpGet]
+        public async Task<IActionResult> GetStudentByIdAsync(int id) 
+        {
+            var result = await studentProfileService.GetProfileByIdAsync(id);
+            return Ok(ApiResponse<ViewStudentProfileDTO>.SuccessResponse(result));
+        }  
+    }
+}
---
+++ StudentManagementSystem.BusinessLayer/Contracts/IStudentProfileService.cs
@@ -0,0 +1,15 @@
+﻿using StudentManagementSystem.BusinessLayer.DTOs.StudentDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.BusinessLayer.Contracts
+{
+    public interface IStudentProfileService
+    {
+        Task<ViewStudentProfileDTO> CreateProfileAsync(CreateStudentDTO studentDTO);
+        Task<ViewStudentProfileDTO> GetProfileByIdAsync(int id);
+    }
+}
